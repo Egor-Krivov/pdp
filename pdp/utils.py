0d59c96ff29553842c4c0070e2cee7e08349cca1
Egor Krivov <egor.krivov@frtk.ru>
2017-08-11 04:48:35 +0300

    new combine batches were added

--- pdp/utils.py
+++ pdp/utils.py
@@ -1,3 +1,10 @@
+import numpy as np
+
+
+def combine_batches(inputs):
+    return [np.array(o) for o in zip(*inputs)]
+
+
 def pack_args(func):
     def new_func(inputs):
         return func(*inputs)
