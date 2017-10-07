f8b4dc3b4fd9f7b5b67e533ac15ae50ab85c0476
Egor krivov <egor.krivov@frtk.ru>
2017-10-07 21:55:23 +0300

    log fixed

--- pdp/log.py
+++ pdp/log.py
@@ -3,4 +3,5 @@ import logging
 logging.basicConfig(
     filename='pdp.log', level=logging.ERROR, filemode='w',
     format='%(levelno)d [%(asctime)s.%(msecs)03d] %(message)s',
-    datefmt='%H:%M:%S')
+    datefmt='%H:%M:%S'
+)
